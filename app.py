9043b667fe936ac2e5dfb0a6beda3828c5eb92d6
gigingeorgebga <gigin.george@thebga.io>
2026-02-26 17:32:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,7 @@ else:
                     user_df.loc[user_df['Email'].str.lower() == st.session_state['email'].lower(), 'Password'] = new_p
                     save_data(user_df, "users")
                     st.success("Password updated! Please wait for refresh...")
+                    st.rerun() # <--- THIS IS THE MISSING NUDGE
                     st.rerun() # <--- ADD THIS LINE
                     st.rerun()
                 else:
